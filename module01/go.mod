2666c210b2250ae0db40b611fe2b21bf6d559cb8
benny <178542285@qq.com>
2022-09-17 10:54:42 +0800

    第一次作业

---
+++ module01/go.mod
@@ -0,0 +1,3 @@
+module mycncamp/module01
+
+go 1.18
---
+++ module01/module01_test.go
@@ -0,0 +1,56 @@
+/*
+第一次作业
+*/
+package module01
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+/*
+编写一个小程序：
+给定一个字符串数组
+[“I”,“am”,“stupid”,“and”,“weak”]
+用 for 循环遍历该数组并修改为
+[“I”,“am”,“smart”,“and”,“strong”]
+*/
+func Test01(t *testing.T) {
+	strArr := [5]string{"I", "am", "stupid", "and", "weak"}
+	for index, val := range strArr {
+		fmt.Printf("索引:%v, 值:%v\n", index, val)
+		if val == "stupid" {
+			strArr[index] = "smart"
+		} else if val == "weak" {
+			strArr[index] = "strong"
+		}
+	}
+	fmt.Printf("修改后的数组值：%v", strArr)
+}
+
+/**
+基于 Channel 编写一个简单的单线程生产者消费者模型：
+
+队列：
+队列长度 10，队列元素类型为 int
+生产者：
+每 1 秒往队列中放入一个类型为 int 的元素，队列满时生产者可以阻塞
+消费者：
+每一秒从队列中获取一个元素并打印，队列为空时消费者阻塞
+*/
+func Test02(t *testing.T) {
+	intChan := make(chan int, 10)
+	go func() {
+		for true {
+			intChan <- rand.Intn(10)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for i := 0; ; i++ {
+		val := <-intChan
+		println("从channel中获取到值:", val)
+	}
+}
